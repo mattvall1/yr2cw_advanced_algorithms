214ffae02ad67376e028ed7ede942675965d282b
Matthew Vallance <matthew.vallance@outlook.com>
2023-10-24 06:42:07 +0100

    Data reading adjustments

--- read_data.py
+++ read_data.py
@@ -1,5 +1,6 @@
 import csv
 
+
 # Get data from CSV and format into appropriate data structure
 def get_data():
     # Open and read CSV file
@@ -15,20 +16,15 @@ def get_data():
         for line in data:
             # Add connections with times to an array
             # INCL TEMPORARY 5 ITEM LIMIT
-            if line[2] != '' and temp_count < 4:
+            if line[2] != '' and temp_count < 40000:
                 station_data.append(line)
                 temp_count += 1
 
-            # Create a list of stations with a key to represent them
+            # Create a list of stations with an id to represent them
             if line[2] == '':
-                # Format: [int: key, str: line, str: station]
+                # Format: [int: id, str: line, str: station]
                 station_list.append([int(count), line[0], line[1]])
 
             count += 1
 
-
-
-
-
         return [station_data, station_list]
-
