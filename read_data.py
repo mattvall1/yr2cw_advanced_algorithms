cca01d6d955ea354c0b7d40453f3d4c84d68a2fb
yoyoqq <yagolxuchen@gmail.com>
2023-10-27 00:03:42 +0100

    Commented the files
    Added comments and things to consider in the code
    I think there are some bugs, check out the comments
    you're going in the right track gj! keep it going

--- read_data.py
+++ read_data.py
@@ -4,7 +4,7 @@
     Date: 16/10/23
 """
 import csv
-
+import unittest
 
 # Get data from CSV and format into appropriate data structure
 def get_data():
@@ -19,8 +19,9 @@ def get_data():
         station_data = []
         station_list = []
         for line in data:
+            # print(line)
             # Add connections with times to an array
-            # INCL TEMPORARY 5 ITEM LIMIT
+            # ! INCL TEMPORARY 5 ITEM LIMIT
             if line[2] != '' and temp_count < 4:
                 # Format: [str: start station, str: dest. station, int: time between stations]
                 station_data.append([line[1], line[2], int(line[3])])
@@ -34,3 +35,27 @@ def get_data():
             count += 1
 
         return [station_data, station_list]
+
+
+# TODO: test the correctness of the data
+class Test_get_data_method(unittest.TestCase):
+    """
+    considerations on the testing: (are we handling the following points?)
+        1. This data may have inaccuracies or missing information. It's up to you to determine
+how to address and report these issues in your study
+        2. The dataset excludes durations associated with waiting times at stations and the
+process of passengers boarding or alighting.
+    """
+    def __init__(self, station_data, station_list):
+        pass
+    
+# Run code if called directly
+if __name__ == '__main__':
+    station_data, station_list = get_data()
+    # print(station_data)
+    print(station_list)
+    """
+    ? so the line is not needed? there are many ways to get from one statoin to another by chanignig lines
+    e.g. you can take picadilly or victoria line. do you need to show it at the end if you changed lines or the line taht you are using?
+    """
+    
--- task_1.py
+++ task_1.py
@@ -13,16 +13,27 @@ from clrs_library_slim import dijkstra
 print('Preparing data...')
 
 # Get required data
-station_data = read_data.get_data()[0]
-station_list = read_data.get_data()[1]
+station_data = read_data.get_data()[0]  # list[[start, dest, time]]
+station_list = read_data.get_data()[1]  # list[[index, line, station]]
 
-print(station_list)
 print(station_data)
+# print(station_list)
 
 # Put station data into adjacency matrix - Adjust first parameter according to size of dataset - WE CAN DO THIS AUTOMATICALLY LATER
 # TODO: Use len(station_list) here, after testing
-station_data_graph = AdjacencyListGraph(5, False, True)
+# i think the first paremeter it asks for the verticies which means the nodes taht the graph has. (stations that you can go) so that would not work
+# ? are you trying to use len(station_list) to take get all the vertices (nodes)? if thats the case, 
+# ? are you sure the len(staiotn_list) will work? that list prints out almost all the values that you took from the csv. for exmaple in station_list, you have:
+#  [1, 'Bakerloo', 'Kenton']     AND      [2, 'Bakerloo', 'South Kenton']
+# do you want to take those lists twice? waht if from bakerloo you can go to anotehr statoin? that would be:
+#  [1, 'Bakerloo', 'Kenton'], [2, 'Bakerloo', 'South Kenton'], [bakerlloo, ""], ["", bakerloo] 
+# the result for that would be 4 nodes that you added instead of 3!! 
+station_data_graph = AdjacencyListGraph(5, False, True)     
+    # ? for the first paremeter, you need to know how many vertices (nodes) it has? how can you get all the vertices? maybe, put all the name of the stations in a set? (ONLY THE NAME!) and use length: len(set(all the stations))
+    # ? for the second parameter, are you sure you want and have a directed graph? I think it should work if its directed. but when you take a train from greeniwch to cutty you can come back and that it is reflected on the list
 
+# insert data to the graph
+# ! this needs more explanation
 for station_details in station_data:
     # Get station keys to insert into matrix
     for station in station_list:
@@ -31,13 +42,13 @@ for station_details in station_data:
         elif station_details[1] == station[2]:
             station_id_v = station[0]
 
-    # print(station_id_u)
-    # print(station_id_v)
-    # print(station_details)
+    print(station_id_u)
+    print(station_id_v)
+    print(station_details)
 
     station_data_graph.insert_edge(station_id_u, station_id_v, int(station_details[2]))
 
-print(station_data_graph.get_edge_list())
+# print(station_data_graph.get_edge_list())
 
 # Gather route information from the customer
 starting_station = str(input('Input a starting station: '))
@@ -66,7 +77,7 @@ for station in station_list:
 d, pi = dijkstra(station_data_graph, )
 
 
-print(dijkstra.dijkstra(station_data_graph, 1))
+# print(dijkstra.dijkstra(station_data_graph, 1))
 
 # TODO: Display routing here
 
