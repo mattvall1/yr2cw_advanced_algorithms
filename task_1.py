f5ad16048a4c6cb45119a38557e973140d523249
Matthew Vallance <matthew.vallance@outlook.com>
2023-10-30 18:45:11 +0000

    Completed data import adjustments

--- task_1.py
+++ task_1.py
@@ -3,10 +3,13 @@
     Purpose: Task 1 - Data added to graph and shortest path algorithm
     Date: 25/10/23
 """
+import numpy as np
+
 # Import relevant libraries
 import read_data
 from clrs_library_slim.adjacency_list_graph import AdjacencyListGraph
 from clrs_library_slim import dijkstra
+from testing import data_to_csv
 
 # Tell user the program has started
 print('Preparing data...')
@@ -32,8 +35,45 @@ for connection in station_data:
         if connection[2] == station[1]:
             station_id_v = station[0]
 
-    station_edges.append([station_id_u, station_id_v, int(connection[3])])
-    # station_data_graph.insert_edge(station_id_u, station_id_v, int(connection[3]))
+    # Check edge against all previous results and add to station_edges if no duplicates are found
+    station_edges.append([sorted([station_id_u, station_id_v]), int(connection[3])])
+
+# Sort all edges so we can remove duplicates and find the shortest time between the duplicates
+station_edges = sorted(station_edges)
+station_edges_cleaned = []
+for i in range(1, len(station_edges)):
+    # Get the shortest routes between each station (regardless of line)
+    if station_edges[i][0] != station_edges[i - 1][0]:
+        station_edges_cleaned.append([[station_edges[i][0][0], station_edges[i][0][1]], station_edges[i][1]])
+    else:
+        # Keep edges which have a smaller weight than the last item, or if they are equal (we remove these later)
+        if station_edges[i][1] < station_edges[i - 1][1] or station_edges[i][1] == station_edges[i - 1][1]:
+            station_edges_cleaned.append([[station_edges[i][0][0], station_edges[i][0][1]], station_edges[i][1]])
+
+# Run sorting algorithm twice to make sure we catch everything
+# TODO: Make this less crap
+station_edges_cleaned_2 = []
+for i in range(1, len(station_edges_cleaned)):
+    # Get the shortest routes between each station (regardless of line)
+    if station_edges_cleaned[i][0] != station_edges_cleaned[i - 1][0]:
+        station_edges_cleaned_2.append([station_edges_cleaned[i][0][0], station_edges_cleaned[i][0][1], station_edges_cleaned[i][1]])
+    else:
+        # Keep edges which have a smaller weight than the last item, or if they are equal (we remove these later)
+        if station_edges_cleaned[i][1] < station_edges_cleaned[i - 1][1] or station_edges_cleaned[i][1] == station_edges_cleaned[i - 1][1]:
+            station_edges_cleaned_2.append([station_edges_cleaned[i][0][0], station_edges_cleaned[i][0][1], station_edges_cleaned[i][1]])
+
+# Convert list of edges to tuples to remove duplicates, then convert back to list of lists
+station_edges_set = set(tuple(x) for x in station_edges_cleaned_2)
+station_edges_cleaned_2 = [list(x) for x in station_edges_set]
+
+data_to_csv.write_to_csv(sorted(station_edges_cleaned_2), 'edges')
+# exit()
+
+# Add all station edges to graph
+for edge in station_edges_cleaned_2:
+    # Remove self loops before inserting edge
+    if edge[0] != edge[1]:
+        station_data_graph.insert_edge(edge[0], edge[1], edge[2])
 
 
 # Gather route information from the customer
