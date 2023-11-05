9caf8726e0f693ea9fbf49ce857ae2ca359ca3c4
Matthew Vallance <matthew.vallance@outlook.com>
2023-11-05 14:53:19 +0000

    Added TODO

--- extras_data_visualization.py
+++ extras_data_visualization.py
@@ -7,6 +7,8 @@ import networkx
 import matplotlib as mpl
 import task_1
 
+# TODO: Use this: https://stackoverflow.com/questions/24653190/how-to-create-a-graph-using-a-csv-file-data
+
 station_graph = networkx.from_dict_of_lists(task_1.station_data_graph)
 
 networkx.draw_circular(station_graph, with_labels=True, node_color="magenta", edge_color="gray")
