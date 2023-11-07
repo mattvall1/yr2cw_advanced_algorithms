ff219a5c1579d03885e603c81a857108c632be0f
Matthew Vallance <matthew.vallance@outlook.com>
2023-11-07 16:02:15 +0000

    Task_1 works with 2 lines

--- read_data.py
+++ read_data.py
@@ -9,28 +9,12 @@ import csv
 def remove_duplicate_stations(station_list) -> list:
     return list(set(station_list))
 
-# return the nodes with an index 
-# ? is there any way on how you wanna index all the nodes?, e.g. cutty sark must be index 5? ig it doesnt matter 
-def add_id_to_stations(station_list_no_duplicates:list) -> list:
-    """
-    input -> nodes in the graph
-    output -> indexed station
-    """
-    station_list_with_ids = []
-    count = 0
-    for station in station_list_no_duplicates:
-        # ? why int(count), count is already an integer
-        # station_list_with_ids.append([int(count), station.rstrip().replace('\'', '')])
-        station_list_with_ids.append([count, station.rstrip().replace('\'', '')])
-        count += 1
-    return station_list_with_ids
-
 # Get data from CSV and format into appropriate data structure - using above functions
 def get_data():
     # Open and read CSV file
     # with open('data/small_data.csv', 'r') as file: # Uncomment for testing on one line - Bakerloo
-    # with open('data/medium_data.csv', 'r') as file: # Uncomment for testing on two lines - Bakerloo + Central
-    with open('data/london_underground_data.csv', 'r') as file:
+    with open('data/medium_data.csv', 'r') as file: # Uncomment for testing on two lines - Bakerloo + Central
+    # with open('data/london_underground_data.csv', 'r') as file:
         # Read CSV file
         data = csv.reader(file)
 
@@ -47,82 +31,9 @@ def get_data():
 
             # Create a list of stations
             if line[2] == '':
-                station_list.append(line[1].rstrip())
+                station_list.append(line[1].rstrip().replace('\'', ''))
 
     # Remove duplicate stations
     station_list_no_duplicates = remove_duplicate_stations(station_list)  # return the nodes in the graph
 
-    # Add IDs to stations
-    station_list_with_ids = add_id_to_stations(station_list_no_duplicates)
-
-
-    # Assign the station IDs to each connection, to create edges to insert into the graph
-    station_edges = []
-    all_possible_edges = set()
-    for start, stop, time in station_data:
-        all_possible_edges.add((start, stop, time))
-        all_possible_edges.add((stop, start, time))
-    all_possible_edges = list(all_possible_edges)
-    print(all_possible_edges)
-    """
-    but you have to do it with indexes so its tricky if you use the variable station_list_with_ids with a list 
-    why not a dict() and then you map the name with its index?
-
-    so to put all the stations with its index lets have a variable called staions which is all the nodes in the graph
-    assumptions: a station only appear once and only once (cannot be: ["cutty", "cutty"])
-
-    stations = []
-    map_station_idx = defaultdict(int)
-    count = 0
-
-    # put values on map
-    for s in station:
-        map_station_idx[s] = count  # assign -> "cutty" = 0         then        "greenwich" = 1     etc...
-        count += 1
-
-    # now that we have the values indexed, we can map the any staiton in O(1) time 
-    # lets use the undirected graph code that i wrote above
-    station_edges = []  # ['Bethnal Green', 'Liverpool Street', '3']
-    all_possible_edges = set()
-    for start, stop, time in station_data:
-        all_possible_edges.add((map_station_idx[start], map_station_idx[stop], time))
-        all_possible_edges.add((map_station_idx[stop], map_station_idx[start], time))
-    all_possible_edges = list(all_possible_edges)
-
-    # now all_possible_edges is your final result 
-    # in your code you are calling it station_edges
-    # i kinda bruted forced the solution using set, not sure if there is a better implementation, if you find it out please let me know :)
-    """
-
-    # station_edges = []
-    # for connection in station_data:
-    #     # Get station edges to insert into array
-    #     start = 0
-    #     dest = 0
-    #     for station in station_list_with_ids:
-    #         # Start
-    #         if connection[0] == station[1]:
-    #             start = station[0]
-    #         # Destination
-    #         if connection[1] == station[1]:
-    #             dest = station[0]
-    #
-    #     # Make sure each edge is in the same order - this is so we can get the smallest weight easier
-    #     if start < dest:
-    #         station_edges.append([start, dest, int(connection[2])])
-    #     else:
-    #         station_edges.append([dest, start, int(connection[2])])
-    #
-    # # Remove duplicate edges
-    # station_edges = [list(x) for x in set(tuple(x) for x in station_edges)]
-    # station_edges = sorted(station_edges)
-    # station_edges_duplicates = []
-    # for i in range(0, len(station_edges)):
-    #     # Get a list of the duplicated edges with the higher weight
-    #     if station_edges[i][0] == station_edges[i - 1][0] and station_edges[i][1] == station_edges[i - 1][1]:
-    #         station_edges_duplicates.append(station_edges[i])
-    # for station_edge_duplicate in station_edges_duplicates:
-    #     station_edges.remove(station_edge_duplicate)
-    # print(station_edges)
-
-    return [station_data, all_possible_edges, station_list_with_ids]
+    return [station_list_no_duplicates, station_data]
--- task_1.py
+++ task_1.py
@@ -1,96 +1,21 @@
-"""
-    Author: Matthew Vallance 001225832
-    Purpose: Task 1 - Data added to graph and shortest path algorithm
-    Date: 25/10/23
-"""
-from testing import testing_functions
-
-# Import relevant libraries
-import read_data
 from clrs_library_slim.adjacency_list_graph import AdjacencyListGraph
-from clrs_library_slim import dijkstra
+from clrs_library_slim.dijkstra import dijkstra
+import read_data
+import testing.testing_functions as testing_functions
 
-# Tell user the program has started
-print('Preparing data...')
-
-# Get required data
-station_data, station_edges, station_list = read_data.get_data()
-
-# Put station data into adjacency list graph - Adjust first parameter according to size of dataset
-station_data_graph = AdjacencyListGraph(len(station_list), False, True)
+station_list, station_data = read_data.get_data()
 
 
-# Add all station edges to graph
-for edge in station_edges:
-    station_data_graph.insert_edge(edge[0], edge[1], edge[2])
+# Textbook example.
+vertices = station_list
+edges = station_data
+# print(edges)
+testing_functions.write_to_csv(edges, 'edges')
+# exit()
+graph1 = AdjacencyListGraph(len(vertices), False, True)
 
-
-testing_functions.get_graph_csv(station_data_graph, station_list)
-
-# Gather route information from the customer
-starting_station = str(input('Input a starting station: '))
-dest_station = str(input('Input a destination station: '))
-
-# Get stations from users input
-# TODO - IF TIME: Add case insensitivity here/Checks on innacurate data entry
-for station in station_list:
-    # Get starting station id
-    if starting_station == station[1]:
-        # Format: [int: id, str: station name]
-        starting_station = [station[0], station[1]]
-        found_match_s = True
-    else:
-        found_match_s = False
-
-    # Get destination station id
-    if dest_station == station[1]:
-        # Format: [int: id, str: station name]
-        dest_station = [station[0], station[1]]
-        found_match_d = True
-    else:
-        found_match_d = False
-
-# Run algorithm here to find shortest path
-d, pi = dijkstra.dijkstra(station_data_graph, station[0])
-
-print(d)
-print(pi)
-
-# Reverse d, pi
-d.reverse()
-pi.reverse()
-
-# Find the indexes of both station in pi and put the smallest index first - we also want to reverse the output to display the order in which the user imputted stations
-reverse_output = False
-if pi.index(starting_station[0]) < pi.index(dest_station[0]):
-    start_index = pi.index(starting_station[0])
-    dest_index = pi.index(dest_station[0])
-else:
-    start_index = pi.index(dest_station[0])
-    dest_index = pi.index(starting_station[0])
-    reverse_output = True
-
-# Get weight (minutes) between the stations from d - we don't want to include the station after, so index - 1 is used here
-minutes_between = d[dest_index - 1]
-
-# Get sublist of pi to find the route we're taking
-stations_between = pi[start_index:dest_index + 1]
-
-# Get all station names of sublist - this should return the routing for the users station choices
-route = []
-for station in stations_between:
-    for station_details in station_list:
-        if station_details[0] == station:
-            route.append(station_details[1])
-
-# Reverse outputs to display in the correct order
-if reverse_output == True:
-    route.reverse()
-
-# Display routing
-print('The shortest route for the given stations is: ' + ' -> '.join(route))
-
-# Display time to get from start to dest
-print('This will take ' + str(minutes_between) + ' minutes.')
-
-
+for edge in edges:
+    graph1.insert_edge(vertices.index(edge[0]), vertices.index(edge[1]), edge[2])
+d, pi = dijkstra(graph1, vertices.index('Charing Cross')) # Starting station here
+for i in range(len(vertices)):
+    print(vertices[i] + ": d = " + str(d[i]) + ", pi = " + ("None" if pi[i] is None else vertices[pi[i]]))
