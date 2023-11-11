7bb722fdf8391a9b362f9c9a692192b9bf129bea
Matthew Vallance <matthew.vallance@outlook.com>
2023-11-11 15:17:54 +0000

    Checkpoint - Working with 1 line, route needs to be determined - see comments

--- task_1.py
+++ task_1.py
@@ -40,6 +40,54 @@ for edge in edges:
 testing_functions.get_graph_csv(underground_graph, vertices)
 exit()
 # Run Dijkstra's algorithm from the clrs library to find the shortest route to all stations based on user input
-d, pi = dijkstra(underground_graph, vertices.index(start_station)) # Starting station here
+d, pi = dijkstra(underground_graph, vertices.index(start_station))
+d_dest_station = 0
 for i in range(len(vertices)):
     print(vertices[i] + ": d = " + str(d[i]) + ", pi = " + ("None" if pi[i] is None else vertices[pi[i]]))
+    # Get d for destination station
+    if str(vertices[i]) == str(dest_station):
+        d_dest_station = d[i]
+        # print(vertices[pi[i]])
+
+# Change 'None' values in pi to -1 for easier coding later
+pi = [-1 if x is None else x for x in pi]
+
+
+# Get route - Traverse backwards?
+"""
+This should return as following, regardless of order the user inputs the data
+    Test with Baker Street -> Edgware road. Should return: Baker Street -> Marylebone -> Edgware Road
+    Test with Edgware road -> Baker Street. Should return: Edgware Road -> Marylebone -> Baker Street 
+    
+    Test with Edgware road -> Regents park. Should return: Edgware Road -> Marylebone -> Baker Street -> Regents park
+    Test with Regents park -> Edgware road. Should return: Regents park -> Baker Street -> Marylebone -> Edgware Road
+"""
+route = [start_station]
+all_stations_added = False
+next_station_to_find = dest_station
+while not all_stations_added:
+    print(next_station_to_find)
+    for i in range(len(vertices)):
+        #    d is time from start station
+        #     when = dest    | = time 2 dest   | station before this one
+        # print(vertices[i],  str(d[i]),      vertices[pi[i]])
+
+        # Find the next station along the line and add to route
+        if vertices[i] == next_station_to_find and next_station_to_find not in route:
+            route.append(vertices[pi[i]])
+            next_station_to_find = vertices[pi[i]]
+            print(next_station_to_find)
+
+        # When the next station is the destination, stop the loop
+        if next_station_to_find == dest_station:
+            all_stations_added = True
+
+# Add destination station to end of route
+if dest_station not in route:
+    route.append(dest_station)
+
+# Display routing
+print('The shortest route for the given stations is: ' + ' -> '.join(route))
+
+# Display time to get from start to dest
+print('This will take ' + str(d_dest_station) + ' minutes.')
