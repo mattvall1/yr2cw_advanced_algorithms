2fc48a32a4e1f7bb6813f50e505da4265bc0c0d9
Matthew Vallance <matthew.vallance@outlook.com>
2023-11-12 19:36:35 +0000

    In progress - Dijkstra's algorithm replaced with Bellman Ford

--- task_3.py
+++ task_3.py
@@ -1,11 +1,11 @@
 """
     Author: Matthew Vallance 001225832
-    Purpose:
+    Purpose: Task 3 - Revised task_2 but with the Bellman Ford Algorithm
     Date: 20/10/23
 """
 
 from clrs_library_slim.adjacency_list_graph import AdjacencyListGraph
-from clrs_library_slim.dijkstra import dijkstra
+from clrs_library_slim.bellman_ford import bellman_ford
 import read_data
 
 # Get data with appropriate variable names
@@ -28,11 +28,11 @@ for edge in edges:
         underground_graph.insert_edge(vertices.index(edge[0]), vertices.index(edge[1]), edge[2])
 
 # Run Dijkstra's algorithm from the clrs library to find the shortest route to all stations based on user input
-d, pi = dijkstra(underground_graph, vertices.index(start_station))
-dijkstra_outputs = []
+d, pi, negative_weight_cycle = bellman_ford(underground_graph, vertices.index(start_station))
+bellman_outputs = []
 for i in range(len(vertices)):
     # Create a sensible data structure of the output
-    dijkstra_outputs.append({'dest': vertices[i], 'd': d[i], 'pi': ("None" if pi[i] is None else vertices[pi[i]])})
+    bellman_outputs.append({'dest': vertices[i], 'd': d[i], 'pi': ("None" if pi[i] is None else vertices[pi[i]])})
 
 
 # Get route by looking for each predecessor in out Dijkstra's output
@@ -41,14 +41,14 @@ all_stations_added = False
 next_station_to_find = dest_station
 station_count = 0
 while all_stations_added is False:
-    for dijkstra_output in dijkstra_outputs:
-        if dijkstra_output['dest'] == str(next_station_to_find):
+    for bellman_output in bellman_outputs:
+        if bellman_output['dest'] == str(next_station_to_find):
             # Add each station to route list to print later
-            route.append(dijkstra_output['dest'])
-            next_station_to_find = dijkstra_output['pi']
+            route.append(bellman_output['dest'])
+            next_station_to_find = bellman_output['pi']
             station_count += 1
             # Set all_stations_added to True, this breaks the loop as we have all the details needed
-            if dijkstra_output['pi'] == 'None':
+            if bellman_output['pi'] == 'None':
                 all_stations_added = True
 
 # TODO: Need to reverse route array at end - if needed depending on start and destination order
