98656f4b6ae9c0eaa23d03c8eca5736d19626665
Matthew Vallance <matthew.vallance@outlook.com>
2023-11-16 16:59:37 +0000

    Removed useless code

--- task_4.py
+++ task_4.py
@@ -26,7 +26,5 @@ for edge in removed_edges:
     print(vertices[edge[0]] + ' -- ' + vertices[edge[1]])
 
 # TODO: Ask user to input two stations, so we can check feasibility of removing the link between them
-start_station = input('Input starting station: ')
-dest_station = input('Input destination station: ')
 
 
