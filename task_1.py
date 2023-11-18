dc64984221c8cb4e66a6649d8020530d8f28fa4c
Matthew Vallance <matthew.vallance@outlook.com>
2023-11-18 14:49:40 +0000

    PEP8 fixes

--- task_1.py
+++ task_1.py
@@ -10,9 +10,9 @@ import utils
 
 
 # Function for task_1 algorithm code
-def task_1_algorithm(underground_graph, vertices, start_station, dest_station):
+def task_1_algorithm(graph, vertices, start, dest):
     # Run Dijkstra's algorithm from the clrs library to find the shortest route to all stations based on user input
-    d, pi = dijkstra(underground_graph, vertices.index(start_station))
+    d, pi = dijkstra(graph, vertices.index(start))
     d_dest_station = 0
     dijkstra_outputs = []
     for i in range(len(vertices)):
@@ -20,13 +20,13 @@ def task_1_algorithm(underground_graph, vertices, start_station, dest_station):
         dijkstra_outputs.append({'dest': vertices[i], 'd': d[i], 'pi': ("None" if pi[i] is None else vertices[pi[i]])})
 
         # Get d for destination station
-        if str(vertices[i]) == str(dest_station):
+        if str(vertices[i]) == str(dest):
             d_dest_station = d[i]
 
     # Get route by looking for each predecessor in shortest path output
     route = []
     all_stations_added = False
-    next_station_to_find = dest_station
+    next_station_to_find = dest
     while all_stations_added is False:
         for dijkstra_output in dijkstra_outputs:
             if dijkstra_output['dest'] == str(next_station_to_find):
@@ -43,16 +43,16 @@ def task_1_algorithm(underground_graph, vertices, start_station, dest_station):
 # Only run this code if its task_1 that is run (prevents issue where we're reusing this code in task_4)
 if __name__ == "__main__":
     # Get graph and vertices
-    underground_graph, vertices = data_processing.get_data()
+    underground_graph, underground_vertices = data_processing.get_data()
 
     # Get station inputs from the user
-    start_station, dest_station = utils.get_stations_from_user(vertices)
+    start_station, dest_station = utils.get_stations_from_user(underground_vertices)
 
-    route, d_dest_station = task_1_algorithm(underground_graph, vertices, start_station, dest_station)
+    route, dest_time = task_1_algorithm(underground_graph, underground_vertices, start_station, dest_station)
 
     # TODO: Need to reverse route array at end - if needed depending on start and destination order
     # Display routing
     print('The shortest route for the given stations is: ' + ' -> '.join(route))
 
     # Display time to get from start to dest
-    print('This will take ' + str(d_dest_station) + ' minutes.')
+    print('This will take ' + str(dest_time) + ' minutes.')
